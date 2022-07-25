95396b043e39543aae686df5286493d993dea3e3
David Kamer <davidkamer@Davids-MacBook-Pro-2.local>
2022-07-25 17:34:01 -0400

    add clean to use manifest for env removal

--- clean.js
+++ clean.js
@@ -1,8 +1,16 @@
 const fs = require('fs')
-
+const manifest = require('./manifest.json')
+const { startsWith } = manifest
 const env = fs.readFileSync('./.env').toString('utf-8')
-console.log({ env })
-const array = env.split('\n').reduce((acc, val) => ([...acc, ...(!!val.startsWith('CM') || !!val.startsWith('ARGO') ? [] : [val])]), [])
+const clean = val => {
+  if (startsWith) {
+    const flagStartsWith = startsWith.some(test => val.startsWith(test))
+    if (flagStartsWith) return true
+  }
+  // TODO - add other basic search functions to manifest and test
+  return false
+}
+const array = env.split('\n').reduce((acc, val) => ([...acc, ...(clean(val) ? [] : [val])]), [])
 const s = array.join('\n')
 
 fs.writeFileSync('.env', s)
